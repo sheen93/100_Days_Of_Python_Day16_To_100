2d64bcb47d5baf55d58f55d22af5ba39934d3676
Sheen <msheen.ameen@gmail.com>
2025-12-17 23:57:05 -0500

    xps 13: day 17 turtle_challenge3 done

--- day 18/Turtle challenge/turlte_challenge3.py
+++ day 18/Turtle challenge/turlte_challenge3.py
@@ -1,28 +1,23 @@
 from turtle import Turtle, Screen
+import random
+
+screen = Screen()
+screen.colormode(255)
 
 tim = Turtle()
+tim.shape("turtle")
 
 sides = 3
 
-for i in range(sides):
-    tim.forward(100)
-    tim.right(360/sides)
+while sides != 11:
+    color1 = random.randint(0, 255)
+    color2 = random.randint(0, 255)
+    color3 = random.randint(0, 255)
+    tim.pencolor(color1,color2,color3)
+    for i in range(sides):
+        tim.forward(100)
+        tim.right(360/sides)
+    sides += 1
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen = Screen()
 screen.exitonclick()
