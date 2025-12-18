0e3be8f88406eb0fc30397d2a4ed0fcfbc4ba7e7
Sheen <msheen.ameen@gmail.com>
2025-12-18 12:31:06 -0500

    xps 13: day 17 turtle_challenge3 done

--- day 18/Turtle challenge/turlte_challenge3.py
+++ day 18/Turtle challenge/turlte_challenge3.py
@@ -6,9 +6,15 @@ screen.colormode(255)
 
 tim = Turtle()
 tim.shape("turtle")
+tim.color("red")
 
+tom = Turtle()
+tom.shape("turtle")
+tom.color("blue")
+tom.teleport(-200,200)
+
+"""tim's way to make different shapes"""
 sides = 3
-
 while sides != 11:
     color1 = random.randint(0, 255)
     color2 = random.randint(0, 255)
@@ -18,6 +24,17 @@ while sides != 11:
         tim.forward(100)
         tim.right(360/sides)
     sides += 1
+
+"""tom's way to make different shapes"""
+def draw_shapes(num_of_sides):
+    angle = 360/num_of_sides
+    for _ in range(num_of_sides):
+        tom.forward(100)
+        tom.right(angle)
+for shape_side_n in range(3,11):
+    draw_shapes(shape_side_n)
+
+
 
 
 screen.exitonclick()
