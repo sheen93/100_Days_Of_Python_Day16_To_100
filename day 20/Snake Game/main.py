2ffa365a68f35f66c824cedfccab611885717310
Muhammad Ameen <sheen93@users.noreply.github.com>
2025-12-29 10:49:36 -0500

    day 20 continue

---
+++ day 20/Snake Game/main.py
@@ -0,0 +1,62 @@
+import turtle
+import time
+
+screen = turtle.Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("Snake Game")
+screen.tracer(0)
+
+starting_positions = [(0,0), (-20,0), (-40,0)]
+
+segments = []
+
+for position in starting_positions:
+    new_segment = turtle.Turtle("square")
+    new_segment.color("white")
+    new_segment.penup()
+    new_segment.goto(position)
+    segments.append(new_segment)
+
+
+
+game_on = True
+
+while game_on:
+    screen.update()
+    time.sleep(0.1)
+    for seg_num in range(len(segments)-1,0,-1):
+        new_x=segments[seg_num-1].xcor()
+        new_y=segments[seg_num-1].ycor()
+        segments[seg_num].goto(new_x,new_y)
+    segments[0].fd(20)
+    segments[0].right(90)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
