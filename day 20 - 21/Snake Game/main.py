515323cc5a45bbe46db6122177a54753091df2cd
Muhammad Ameen <sheen93@users.noreply.github.com>
2025-12-30 10:41:01 -0500

    day 20-21 continue

--- day 20 - 21/Snake Game/main.py
+++ day 20 - 21/Snake Game/main.py
@@ -2,6 +2,7 @@ import turtle
 from snake import Snake
 import time
 from food import Food
+import scoreboard
 
 screen = turtle.Screen()
 screen.setup(width=600, height=600)
@@ -18,7 +19,7 @@ screen.onkey(snake.down, "Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
 
-score = 0
+score_turtle = scoreboard.Scoreboard()
 game_on = True
 
 while game_on:
@@ -30,6 +31,7 @@ while game_on:
     #detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        score_turtle.increase_score()
 
 
 
--- day 20 - 21/Snake Game/scoreboard.py
+++ day 20 - 21/Snake Game/scoreboard.py
@@ -1,6 +1,23 @@
 from turtle import Turtle
+ALIGN ="center"
+FONT = ("Courier", 20, "normal")
 
 class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
+        self.score=0
+        self.color("white")
+        self.hideturtle()
+        self.penup()
+        self.goto(0,260)
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGN, font=FONT)
+
+
+    def increase_score(self):
+        self.score+=1
+        self.clear()
+        self.update_scoreboard()
 
