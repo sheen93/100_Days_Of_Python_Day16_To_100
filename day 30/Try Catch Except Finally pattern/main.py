5d141cceb3653021fa8cc3f000e75efa0d290042
Sheen <msheen.ameen@gmail.com>
2026-04-20 13:44:33 -0400

    day 22 continue

--- day 30/Try Catch Except Finally pattern/main.py
+++ day 30/Try Catch Except Finally pattern/main.py
@@ -3,22 +3,6 @@
 #FileNotFound
 # with open("a_file.txt") as file:
 #     file.read()
-try:
-    file = open("a_file.txt")
-    a_dictionary = {"key":"value"}
-    print(a_dictionary["ke"])
-except FileNotFoundError:
-    file = open("a_file.txt", "w")
-    file.write("Something")
-except KeyError as error_message:
-    print(f"The key {error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
-    print("File was closed")
-
 
 #KeyError
 # a_dictionary = {"key": "value"}
@@ -31,3 +15,28 @@ finally:
 #TypeError
 # text = "abc"
 # print(text + 5)
+
+#Example 1
+# try:
+#     file = open("a_file.txt")
+#     a_dictionary = {"key":"value"}
+#     print(a_dictionary["key"])
+# except FileNotFoundError:
+#     file = open("a_file.txt", "w")
+#     file.write("Something")
+# except KeyError as error_message:
+#     print(f"The key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     raise TypeError("This is a TypeError that I made up")
+
+#Example 2
+height = float(input("Height (in): "))
+weight = int(input("Weight (lbs): "))
+
+if height > 120:
+
+bmi = (weight / (height ** 2)) * 703
+print(bmi)
